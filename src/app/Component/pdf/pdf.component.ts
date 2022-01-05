a6fc253196cd9aba5b314be05195226dd9f48815
Alexandre <alexandre.fernique@etu.umontpellier.fr>
2022-01-05 19:56:47 +0100

    AJout du composant pdf

---
+++ src/app/Component/pdf/pdf.component.ts
@@ -0,0 +1,26 @@
+import {Component, Input} from '@angular/core';
+import {Recipe} from "../../class/recipe";
+
+@Component({
+  selector: 'app-pdf',
+  templateUrl: './pdf.component.html',
+  styleUrls: ['./pdf.component.css']
+})
+export class PdfComponent  {
+  @Input()recipe:Recipe|undefined
+  @Input()cout_fluide:number=0
+  @Input()cout_personnel:number=0
+  @Input()costCharge:boolean=false
+  @Input()marge:number=0
+  @Input()impresionCout:boolean=false
+
+  constructor() { }
+  convertNumber(item:number,decimal:number){
+
+    return Number(item.toFixed(decimal))
+  }
+
+
+
+
+}
